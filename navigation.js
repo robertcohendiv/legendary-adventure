4f5c6a8444b5f1b84a6c35b9244ad8ba1ab0b21b
robertcohen <me@robertdcohen.com>
2023-02-10 16:17:42 -0800

    some js updates

---
+++ navigation.js
@@ -0,0 +1,8 @@
+document.getElementById("myButtonId1").addEventListener("click", function () {
+  window.location.href = "CatnipStock.html";
+});
+
+
+document.getElementById("myButtonId2").addEventListener("click", function () {
+  window.location.href = "FFF.html";
+});
